944b8fd8f8c61e0b3abc2e3e848c97b11c98f9f9
Raffaella Perna <raffaella.p05@gmail.com>
2024-05-26 17:22:02 +0200

    Complex topology

--- topology.py
+++ topology.py
@@ -15,10 +15,10 @@ from mininet.link import TCLink
 
 class SimpleTopology(Topo):
     
-    def __init__( self ):
+    def __init__(self):
 
         # Initialize topology
-        Topo.__init__( self )
+        Topo.__init__(self)
         
         # Create router node
         s1 = self.addSwitch("s1")
@@ -42,7 +42,45 @@ class SimpleTopology(Topo):
 
         self.addLink(s1, s2)
  
-topos = { 'simpletopology': ( lambda: SimpleTopology() ) }
+class ComplexTopology(Topo):
+    
+    def __init__(self):
+
+        # Initialize topology
+        Topo.__init__(self)
+        
+        # Create router node
+        s1 = self.addSwitch("s1")
+        s2 = self.addSwitch("s2")
+        s3 = self.addSwitch("s3")
+
+        # Create host nodes
+        h1 = self.addHost("h1")
+        h2 = self.addHost("h2")
+        h3 = self.addHost("h3")
+        h4 = self.addHost("h4")
+        h5 = self.addHost("h5")
+        h6 = self.addHost("h6")
+        h7 = self.addHost("h7")
+        h8 = self.addHost("h8")
+        h9 = self.addHost("h9")
+
+        # Add links
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s1)
+        self.addLink(h4, s2)
+        self.addLink(h5, s2)
+        self.addLink(h6, s2)
+        self.addLink(h7, s3)
+        self.addLink(h8, s3)
+        self.addLink(h9, s3)
+
+        self.addLink(s1, s2)
+        self.addLink(s2, s3)
+        self.addLink(s1, s3)
+ 
+topos = { 'simpletopology': (lambda: SimpleTopology()), 'complextopology': (lambda: ComplexTopology()) }
 
 start_time = None
 def process_packet(packet, csv_file):
