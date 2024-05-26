e9892c444de7f9a4ddc1bc3fb8b8776bc82d429c
Raffaella Perna <raffaella.p05@gmail.com>
2024-05-26 16:50:05 +0200

    Constant background traffic

--- traffic_generation.py
+++ traffic_generation.py
@@ -33,6 +33,14 @@ def square_traffic(host1, host2, duration):
         host1.cmd(f"iperf -c {host2.IP()} -b {bandwidth}M -t 1 &")
         time.sleep(1)
 
+def constant_traffic(net):
+    while True:
+        for host in net.hosts:
+            for other_host in net.hosts:
+                if other_host != host:
+                    host.cmd(f"iperf -c {other_host.IP()} -b 1M -t 1 &")
+        time.sleep(1)
+
 def generate_traffic(net, duration:int):
 
     # Starting iperf server on hosts
@@ -40,6 +48,14 @@ def generate_traffic(net, duration:int):
     for host in  net.hosts:
         host.cmd("iperf -s &")
 
+    # Start constant traffic in the background
+    print("[INFO] Starting constant background traffic.")
+    constant_traffic_cmd = "while true; do iperf -c {ip} -b 1M -t 1; sleep 1; done &"
+    for host in net.hosts:
+        for other_host in net.hosts:
+            if other_host != host:
+                host.cmd(constant_traffic_cmd.format(ip=other_host.IP()))
+    
     # Periodic traffic routines
     traffic_routines = [linear_traffic, sinusoidal_traffic, sawtooth_traffic, square_traffic]
 
@@ -50,4 +66,5 @@ def generate_traffic(net, duration:int):
     print(f"[INFO] Applying {routine.__name__} between {host1.name} and {host2.name}")
     routine(host1, host2, duration)
 
+    # Wait for the routine to finish
     print("[INFO] Traffic generation completed.")
